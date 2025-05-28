41ae4f8f0aaaaa791d2e9242b4a5c2bafca47e9f
Fabio-Garcia27 <fabio@unipar.br>
2025-05-28 15:15:15 -0300

    Versão 1.0.0.2

---
+++ Menu/js/index.js
@@ -0,0 +1,28 @@
+// Ativando o menu mobile
+
+function mostraMenu(){
+  $('header nav#nav-esq ul#menu-principal').css('display', 'flex');
+  $('header.nav#nav-esq ul#menu-principal').addClass('animate_animated animate_fadeInRight animate_sloq');
+
+  $('header nav#nav-esq ul#icone-menu li#menu').css('display', 'nome');
+  $('header.nav#nav-esq ul#icone-menu li#menuX').css('display', 'flex');
+}  
+
+function esconderMenu(){
+  $('header nav#nav-esq ul#menu-principal').css('display', 'nome');
+
+  $('header nav#nav-esq ul#icone-menu li#menu').css('display', 'flex');
+  $('header.nav#nav-esq ul#icone-menu li#menuX').css('display', 'nome');
+}  
+
+let controle = true;
+
+$('header nav#nav-esq ul#icone-menu li#menu').click(function()){
+  if (controle ==  true){
+      mostraMenu();
+      controle = false;
+  }else{
+      esconderMenu();
+      controle = true;
+  }
+}
