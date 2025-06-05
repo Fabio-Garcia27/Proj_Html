f2d2a46eeff92e6fa48c43f78e1762e192d2db6a
Fabio-Garcia27 <fabio@unipar.br>
2025-06-05 13:48:32 -0300

    Versão 1.0.0.3

--- API_Zero/server.js
+++ API_Zero/server.js
@@ -5,10 +5,63 @@ post criar
 put editar vários
 patch editar um
 delete deletar
+
+1 - Tipo de Rota / Método HTTP
+2 - Endereço
+
+Criar nossa API de Usuários
+1 - Criar usuário
+2 - Listar todos os usuários
+3 - Editar um usuário
+4 - Deletar um usuário
+
+app.get('/usuarios')
+app.post('/usuarios')
+app.put('/usuarios')
+app.patch('/usuarios')
+app.delete('/usuarios')
+
+HTTP Status
+2xx Sucesso
+4xx Erro Cliente
+5xx Erro Servidor
+
+MongoDB
+
+User:- fabio
+Senha:- Wizz4gPc8AFoqOmx
 */
 
 import express from 'express'
-
 const app = express()
 
-app.
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', async (req, res) => {
+
+    await prisma.user.create({
+        data:{
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    users.push(req.body) 
+    //console.log(req.body)    
+    res.status(201).json(req.body)
+})
+
+//request, responde = req, res
+app.get('/usuarios', (req, res) => {
+    res.status(200).json(users)
+    //res.send('Ok, deu bom')
+})
+
+app.listen(3000)
+
