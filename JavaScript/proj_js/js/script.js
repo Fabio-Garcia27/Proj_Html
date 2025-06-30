7ff93f9cd73b9b931571d400dca345b38f599440
Fabio-Garcia27 <fabio@unipar.br>
2025-06-30 10:31:14 -0300

    Versão 1.0.0.1

---
+++ JavaScript/proj_js/js/script.js
@@ -0,0 +1,37 @@
+// tipode variáveis
+
+let idade = 90 // tipo numérico
+
+let nome = "Fábio Garcia" // tipo string
+
+let logado = false // tipo booleano
+
+console.log(idade)
+console.log(nome)
+
+// array 
+let ingredientes = ["farina", "água", "sal", "corante"] // array
+
+console.log(ingredientes) // lista completa
+console.log(ingredientes[3]) // posição pegar na lista
+console.log(ingredientes[3],ingredientes[0]) // duas posições pegar na lista
+
+// objeto 
+
+let personagem = {
+    nome: "Fábio",
+    nivel: 10,
+    forca: 800,
+    magia: 200,
+    vida: 1000,
+    mana: 200
+}
+
+console.log(personagem) // lista completa
+console.log(personagem.magia) // lista 1 item
+
+// funções = input processa output
+
+function conversao() {
+x = 0;
+}
