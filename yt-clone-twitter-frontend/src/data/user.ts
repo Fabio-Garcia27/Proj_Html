8733b54637d67122267dd8043ccee9fe3d103eb0
Fabio-Garcia27 <fabio@unipar.br>
2025-07-03 10:04:13 -0300

    Versão 1.0.0.5

---
+++ yt-clone-twitter-frontend/src/data/user.ts
@@ -0,0 +1,11 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+  slug: 'Garcia',
+  name: 'Fábio Garcia',
+  avatar: 'https://ui-avatars.com/api/?name=Fabio+Garcia&background=007bff&color=FFFFFF&rounded=true',
+  cover: 'https://source.unsplash.com/featured/?travel,bag',
+  bio: 'Administrador e DAS',
+  link: 'https://unipar.br',
+  postCount: 127
+}
