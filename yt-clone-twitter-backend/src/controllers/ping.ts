c3cb08ea6e0db117401122981513948237d57706
Fabio-Garcia27 <fabio@unipar.br>
2025-07-11 13:33:39 -0300

    Versão 1.0.0.2

--- yt-clone-twitter-backend/src/controllers/ping.ts
+++ yt-clone-twitter-backend/src/controllers/ping.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
 export const ping = (req: Request, res: Response) => {
-    res.json
+    res.json({pong: true});
 }
--- yt-clone-twitter-backend/src/server.ts
+++ yt-clone-twitter-backend/src/server.ts
@@ -1,6 +1,7 @@
 import express, { urlencoded } from "express";
 import cors from 'cors';
 import helmet from "helmet";
+import { mainRouter } from "./routers/main";
 
 const server = express();
 server.use(helmet());
@@ -9,6 +10,7 @@ server.use(urlencoded({extended: true}));
 server.use(express.json());
 
 //rotas
+server.use(mainRouter);
 
 server.listen(process.env.PORT || 3000, () => {
     console.log(`Servidor rodando em ${process.env.BASE_URL}`);
