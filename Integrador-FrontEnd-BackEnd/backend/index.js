a476020f11d70718e4a6b4e4e5a77c95b9166059
Fabio-Garcia27 <fabio@unipar.br>
2025-07-18 16:20:07 -0300

    Versão 1.0.0.1

---
+++ Integrador-FrontEnd-BackEnd/backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+const users = [
+    {
+       name: 'Fábio Garcia',
+       age: 53,
+    }
+]
+
+app.get('/usuarios', function (request, response) {
+    response.json[users]
+})
+
+app.post('/usuarios', function (request, response) {
+    console.log(request.body)
+
+    const newUser = request.body
+
+    users.push(newUser)
+
+    response.status(201).json(newUser)
+})
+
+
+app.listen(3001, () => console.log("Servidor rodando"))
