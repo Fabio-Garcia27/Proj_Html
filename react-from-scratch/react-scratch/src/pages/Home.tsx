e966b933373d3694751914926cd4bc10a93b3f0e
Fabio-Garcia27 <fabio@unipar.br>
2025-08-13 18:14:09 -0300

    Versão 1.0.0.1

---
+++ react-from-scratch/react-scratch/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import { Footer } from "../components/Footer";
+import { NavBar } from "../components/NavBar";
+import { Section } from "../components/Section";
+
+
+export function Home() {
+    return (
+        <>
+            <NavBar />
+            <Section />
+            <Footer />
+        
+        </>    
+    )
+}
