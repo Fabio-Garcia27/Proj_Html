dce726443aab1d296b5ce7870a8e7e4d38a1af33
Fabio-Garcia27 <fabio@unipar.br>
2025-08-13 13:52:46 -0300

    Versão 1.0.0.2

--- curso-de-react/src/App.tsx
+++ curso-de-react/src/App.tsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// @ts-expect-error: Missing type declarations for Tasks component in JSX
+import Tasks from "./components/Tasks";
+// @ts-expect-error: Missing type declarations for AddTask component
+import AddTask from "./components/AddTask";
+import { useState } from "react";
+import { v4 } from "uuid";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar Inglês",
+      description: "Estudar inglês para se tornar fluente",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Programação",
+      description:
+        "Estudar programação para se tornar um desenvolvedor full stack",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar Matemática",
+      description: "Estudar matemática para se tonar professor",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId: number) {
+    const newTasks = tasks.map((task) => {
+      // preciso atualizar essa tarefa
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      } else {
+        // não preciso atualizar essa tarefa
+        return task;
+      }
+    });
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId: number) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(title: string, description: string) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    }
+    setTasks([...tasks, newTask]);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask
+          onAddTaskSubmit={onAddTaskSubmit}
+        />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
